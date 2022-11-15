d1a5baa00148b68e7e03de70169adfdfc7cd116c
Raja <114216677+Raja2107@users.noreply.github.com>
2022-11-15 14:20:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://c4.wallpaperflare.com/wallpaper/887/817/276/html-5-html-5-logo-wallpaper-preview.jpg",
+      link: "https://github.com/Raja2107",
+    },
+    {
+      id: 2,
+      img: "https://c4.wallpaperflare.com/wallpaper/241/580/981/css-css3-wallpaper-preview.jpg",
+      link: "https://github.com/Raja2107",
+    },
+    {
+      id: 3,
+      img: "https://www.linkweb.ca/wp-content/uploads/2020/02/bootstrap.jpg",
+      link: "https://github.com/Raja2107",
+    },
+    {
+      id: 4,
+      img: "https://cutewallpaper.org/21/javascript-background-images/What-does-someone-with-no-background-in-computers-need-to-.webp",
+      link: "https://github.com/Raja2107",
+    },
+    {
+      id: 5,
+      img: "https://c4.wallpaperflare.com/wallpaper/294/834/442/reactjs-facebook-javascript-minimalism-wallpaper-preview.jpg",
+      link: "https://github.com/Raja2107",
+    },
+    {
+      id: 6,
+      img: "https://c4.wallpaperflare.com/wallpaper/619/468/16/node-js-javascript-wallpaper-preview.jpg",
+      link: "https://github.com/Raja2107",
+    },
+  ];
